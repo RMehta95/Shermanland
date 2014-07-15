ac095cc9fad3aa3936c491af516d6e0887e0ecc6
Rohan Mehta <rohanmehta2017@u.northwestern.edu>
2014-07-15 15:10:37 -0500

    Separating HTML, CSS, & JS files
    Also added CDN to EaselJS for simplicity

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 // DOCTYPE JAVASCRIPT
 
 
-var c=document.getElementById("myCanvas");
-var ctx=c.getContext("2d");
-var img=document.getElementById("scream");
-ctx.drawImage(img,10,10);
+var canvas, stage, bg;
+
+function init() {
+    canvas = document.getElementById("canvas");
+    // var ctx = canvas.getContext("2d");
+
+
+
+    // var bg = new Image();
+    // bg.src = "shermanlanddd.jpg";
+    // bg.onload = function(){
+    //   ctx.drawImage(this,0,0);
+    // };
+
+    // var pieces = new Image();
+    // pieces.src = "shotglasstest.jpg";
+
+    stage = new createjs.Stage("canvas");
+    // Ticker.setFPS(60);
+    // Ticker.addListener(this);
+    createjs.Ticker.addEventListener("tick", handleTick);
+    bg = new createjs.Bitmap("shermanlanddd.jpg");
+    bg.scaleX = 0.5;
+    bg.scaleY = 0.5;
+    stage.addChild(bg);
+
+}
+
+function handleTick(event) {
+	stage.update();
+}
