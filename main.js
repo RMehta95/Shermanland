2adcb222b9142bb0b4f383fae5ab4c54447752cc
Rohan Mehta <rohanmehta2017@u.northwestern.edu>
2014-08-14 13:16:38 -0500

    Added intro sound, returned selectplayersview

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 //Initial variables
 var canvas; // Linked to canvas on index.html page
 var stage; // EaselJS variable that holds all objects; must add "children".
-var queue; // Loading queue
+var queue; // Loading queue of images and sounds
 
 var main; // Main background on loading screen
 var startB; // Start button in main menu
@@ -79,7 +79,7 @@ var pointArray = [{x:70, y:500},
     {x:239, y:292},
     {x:220, y:318},
     {x:204, y:341},
-    {x:185, y:362},
+    {x:183, y:362},
     {x:160, y:380},
     {x:131, y:388},
     {x:102, y:388},
@@ -96,7 +96,7 @@ var pointArray = [{x:70, y:500},
     {x:166, y:288},
     {x:192, y:276},
     {x:198, y:246},
-    {x:179, y:223},
+    {x:177, y:223},
     {x:150, y:208},
     {x:130, y:184},
     {x:132, y:153},
@@ -109,7 +109,7 @@ var pointArray = [{x:70, y:500},
     {x:328, y:180},
     {x:356, y:193},
     {x:384, y:208},
-    {x:415, y:215},
+    {x:413, y:215},
     {x:445, y:223},
     {x:476, y:228},
     {x:507, y:232},
@@ -135,7 +135,7 @@ var pointArray = [{x:70, y:500},
     {x:355, y:148},
     {x:330, y:132},
     {x:305, y:115},
-    {x:278, y:96},
+    {x:276, y:96},
     {x:249, y:87},
     {x:218, y:85},
     {x:187, y:88},
@@ -153,8 +153,8 @@ var pointArray = [{x:70, y:500},
     {x:148, y:-30},
     {x:172, y:-17},
     {x:196, y:2},
-    {x:222, y:18},
-    {x:248, y:32},
+    {x:220, y:18},
+    {x:246, y:32},
     {x:276, y:42},
     {x:310, y:48}]; 
 // Listing of pieces by color
@@ -165,7 +165,7 @@ var yellow = [pointArray[3],pointArray[10],pointArray[16],pointArray[23],pointAr
 var blue = [pointArray[4],pointArray[11],pointArray[17],pointArray[24],pointArray[30],pointArray[36],pointArray[43],pointArray[49],pointArray[55],pointArray[61],pointArray[67],pointArray[74],pointArray[80],pointArray[86],pointArray[93],pointArray[99],pointArray[106],pointArray[112],pointArray[118],pointArray[124],pointArray[130]];
 var gold = [pointArray[5],pointArray[12],pointArray[18],pointArray[25],pointArray[31],pointArray[37],pointArray[44],pointArray[50],pointArray[56],pointArray[62],pointArray[68],pointArray[75],pointArray[81],pointArray[87],pointArray[94],pointArray[100],pointArray[107],pointArray[113],pointArray[119],pointArray[125],pointArray[131]];
 var green = [pointArray[6],pointArray[13],pointArray[19],pointArray[26],pointArray[32],pointArray[38],pointArray[45],pointArray[51],pointArray[57],pointArray[63],pointArray[70],pointArray[76],pointArray[82],pointArray[88],pointArray[95],pointArray[101],pointArray[108],pointArray[114],pointArray[120],pointArray[126],pointArray[132]];
-//check pointArray121
+
 // Game pieces
 var player1, player2, player3, player4;
 
@@ -198,27 +198,31 @@ function init() {
     queue.installPlugin(createjs.Sound);
     createjs.MotionGuidePlugin.install();
     queue.on("complete", handleComplete, this);
+    // queue.loadFile({id:"duckSauce", src:"BarbraStreisand.mp3"});
     queue.loadManifest([
     			{id:"main", src:"titleview2.jpg"},
     			{id:"startB", src:"startbutton.png"},
-                {id:"selectPlayersView", src:"selectplayersview.jpg"},
+                {id:"selectPlayersView", src:"selectplayersview2.jpg"},
                 {id:"playbutton1", src:"playbutton1.png"},
                 {id:"playbutton2", src:"playbutton2.png"},
                 {id:"playbutton3", src:"playbutton3.png"},
                 {id:"playbutton4", src:"playbutton4.png"},
                 {id:"bgImg", src:"shermanlandboard.jpg"},
                 {id:"player1", src:"willie.png"},
-    			{id:"pushbutton", src:"pushbutton.png"}
+    			{id:"pushbutton", src:"pushbutton.png"},
+                {id:"duckSauce", src:"BarbraStreisand.mp3"}
     		]);
 
 }
 
 function handleComplete(event) {
+    createjs.Sound.play("duckSauce");
+    createjs.Sound.setVolume(0.5);
     main = new createjs.Bitmap(queue.getResult("main"));
     startB = new createjs.Bitmap(queue.getResult("startB"));
     startB.y = 100;
     startB.x = 100;
-    startB.addEventListener("click",loadBoard);
+    startB.addEventListener("click",requestPlayers);
     credits = new createjs.Text("©2014 Sherman Ave. Created by Rohan Mehta.", "14px Arial", "#fff");
     credits.x = 650;
     credits.y = 550;
@@ -230,27 +234,32 @@ function handleComplete(event) {
     stage.update();
 }
 
-// function requestPlayers(event) {
-//     selectPlayersView = new createjs.Bitmap(queue.getResult("selectPlayersView"));
-//     playbutton1 = new createjs.Bitmap(queue.getResult("playbutton1"));
-//     playbutton2 = new createjs.Bitmap(queue.getResult("playbutton2"));
-//     playbutton3 = new createjs.Bitmap(queue.getResult("playbutton3"));
-//     playbutton4 = new createjs.Bitmap(queue.getResult("playbutton4"));
+function requestPlayers(event) {
+    selectPlayersView = new createjs.Bitmap(queue.getResult("selectPlayersView"));
+    playbutton1 = new createjs.Bitmap(queue.getResult("playbutton1"));
+    playbutton1.x = 100;
+    playbutton2 = new createjs.Bitmap(queue.getResult("playbutton2"));
+    playbutton2.x = 100;
+    playbutton3 = new createjs.Bitmap(queue.getResult("playbutton3"));
+    playbutton3.x = 100;
+    playbutton4 = new createjs.Bitmap(queue.getResult("playbutton4"));
+    playbutton4.x = 100;
     
-//     selectPlayers = new createjs.Container();
-//     selectPlayers.addChild(selectPlayersView, playbutton1, playbutton2, playbutton3, playbutton4);
+    selectPlayers = new createjs.Container();
+    selectPlayers.addChild(selectPlayersView, playbutton1, playbutton2, playbutton3, playbutton4);
 
-//     stage.removeChild(titleView);
-//     stage.addChild(selectPlayers);
-//     stage.update();
+    stage.removeChild(titleView);
+    stage.addChild(selectPlayers);
+    stage.update();
 
-//     playbutton1.addEventListener("click", loadBoard);
-//     playbutton2.addEventListener("click", loadBoard);
-//     playbutton3.addEventListener("click", loadBoard);
-//     playbutton4.addEventListener("click", loadBoard);
-// }
+    playbutton1.addEventListener("click", loadBoard);
+    playbutton2.addEventListener("click", loadBoard);
+    playbutton3.addEventListener("click", loadBoard);
+    playbutton4.addEventListener("click", loadBoard);
+}
 
 function loadBoard() {
+    // createjs.Sound.stop("duckSauce");
     createjs.Ticker.addEventListener("tick", handleTick);
     createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
 
@@ -274,8 +283,13 @@ function loadBoard() {
     square = new createjs.Shape();
     square.graphics.beginFill("#000").drawRoundRect(850, 50, 100, 100, 20);
 
+    instructions = new createjs.Text("Push to Move", "24px Impact", "#000");
+    instructions.x = 835;
+    instructions.y = 260;
+
+
     board = new createjs.Container();
-    board.addChild(bg, player1, pushbutton, square);
+    board.addChild(bg, player1, pushbutton, square, instructions);
     
     stage.removeAllChildren(titleView);
     stage.addChild(board);
@@ -374,40 +388,38 @@ return;
 
 function movePlayer(color, currentPos, dest) {
 
-console.log(currentPos);
-console.log(dest);
 if (dest===5) {
     createjs.Tween.get(player1)
         .wait(1000)
         .to(pointArray[5], 1000, createjs.Ease.quadIn)
-        .wait(500)
-        .to(purple[9], 1000, createjs.Ease.bounceIn);
+        .wait(1000)
+        .to(pointArray[59], 1000, createjs.Ease.bounceIn);
 } else if(dest === 134) {
     createjs.Tween.get(player1)
         .wait(1000)
-        .to(pointArray[134], 1000, createjs.Ease.bounceIn);
+        .to(pointArray[134], 1000, createjs.Ease.circIn);
 } else {
     createjs.Tween.get(player1)
         .wait(1000)
         .to({x:pointArray[dest].x,y:pointArray[dest].y}, 1000, createjs.Ease.quadIn);
 }
 this.addEventListener("complete",drinkMessage(dest));
-return;
 }
 
 function drinkMessage(dest) {
 
-if (dest===5) window.setTimeout(alert, 3000, "Woah, you're not getting off that easy. Double shot, no chaser.");
+if (dest===5) window.setTimeout(alert, 5000, "Woah, you're not getting off that easy. Double shot, no chaser.");
 else if (dest===6) window.setTimeout(alert, 3000, "Start off with some bacardi. Don't spill.");
 else if (dest===9) window.setTimeout(alert, 3000,"Let's see it, bartender. Gin on the rocks.");
 else if (dest===15 || dest===20 || dest ===22) window.setTimeout(alert, 3000, "Schnapps on schnapps.");
 else if (dest===28) window.setTimeout(alert, 3000,"Smirnoff. Treat yourself to something nice honey.");
 else if (dest===36 || dest===42) window.setTimeout(alert, 3000,"Take your pick. Skol or... skol.");
 else if (dest===49 || dest===53) window.setTimeout(alert, 3000,"Aye aye. Malibu or Captain Morgan, take your pick.");
-else if (dest===62 || dest===69 || dest===80 || dest===87) window.setTimeout(alert, 3000,"Yee ha, Tennessee whiskey time.");
+else if (dest===62 || dest===69 || dest===80 || dest===87) window.setTimeout(alert, 3000,"Yee-ha, Tennessee whiskey time.");
 else if (dest===89 || dest===92) window.setTimeout(alert, 3000,"Schlapp da bag. 10 seconds minimum.");
 else if (dest===99 || dest===102 || dest===105) window.setTimeout(alert, 3000,"Salt, tequila, lime. Go.");
-else if (dest===115 || dest===127) window.setTimeout(alert, 3000,"Liquor before beer, and you're in the clear. Chug.");
-else if (dest===134) window.setTimeout(alert, 3000,"Keg standdddddd");
+else if (dest===117 || dest===129) window.setTimeout(alert, 3000,"Liquor before beer, and you're in the clear. Chug.");
+else if (dest===134) window.setTimeout(alert, 3000,"If you're stillllll alive, 20 second keg standdddddd.");
+
 }
 
